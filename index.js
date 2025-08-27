be273f6440dd159eb1d485b64a11a65e1d92b7cb
nancyerika04 <nancywouegam@gmail.com>
2025-08-27 20:19:18 +0100

    improve flight summarize

--- index.js
+++ index.js
@@ -3,17 +3,44 @@ import dotenv from "dotenv"
 import cors from "cors"
 import db from "./src/services/firestore.js"
 import nodemailer from "nodemailer"
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import cron from "node-cron";
+
+dotenv.config();
+let latestflightsdata = null;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/summary",(req,res)=>{
+app.post("/flights-data", (req, res)=>{
+    const {flights} = req.body;
+    if(!flights){
+        return res.status(400).json({
+            error:"flights data is require"
+        });}
+        latestflightsdata = flights;
+        console.log("Receive flights data");
+        res.json({status:"flights data receive"});
+});
+
+app.post("/summary",async(req,res)=>{
     const{summary} = req.body;
     if (!summary){
         return res.status(400);
     }
     console.log("summary", summary);
+    try{
+        await db.collection('summaries').add({
+            summary:summary,
+            createdAt:new Date()
+        });
+        console.log("The summary is save");
+    }
+    catch(err){
+            console.error(err);
+            return res.status(500);
+    }
     const transporter = nodemailer.createTransport({
     service:"gmail",
     auth:{
